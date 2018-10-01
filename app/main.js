1dbdac420f671d90b12f27f6ccfb86da4ae1de7e
webarata3 <webarata3@gmail.com>
2018-10-01 18:32:45 +0900

    メニューが出ていなかった

--- app/main.js
+++ app/main.js
@@ -19,130 +19,67 @@ app.on('ready', function() {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
+
+  initMenu();
 });
 
-const template = [
-  {
-    label: 'ファイル',
-    submenu: [
-      {
-        label: 'エクスポート',
-        submenu: [
-          {
-            label: 'JSONファイル',
-            click() {
-              exportFile();
-            }
-          },
-          {
-            label: 'HTMLファイル',
-            click() {
-              // TODO
-            }
-          }
-        ]
-      },
-      {
-        label: 'インポート',
-        submenu: [
-          {
-            label: 'JSONファイル',
-            click() {
-              importFile();
-            }
-          }
-        ]
-      }
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      {role: 'undo'},
-      {role: 'redo'},
-      {type: 'separator'},
-      {role: 'cut'},
-      {role: 'copy'},
-      {role: 'paste'},
-      {role: 'pasteandmatchstyle'},
-      {role: 'delete'},
-      {role: 'selectall'}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {role: 'reload'},
-      {role: 'forcereload'},
-      {role: 'toggledevtools'},
-      {type: 'separator'},
-      {role: 'resetzoom'},
-      {role: 'zoomin'},
-      {role: 'zoomout'},
-      {type: 'separator'},
-      {role: 'togglefullscreen'}
-    ]
-  },
-  {
-    role: 'window',
-    submenu: [
-      {role: 'minimize'},
-      {role: 'close'}
-    ]
-  },
-  {
-    role: 'help',
-    submenu: [
-      {
-        label: 'Learn More',
-        click() {
-          require('electron').shell.openExternal('https://electronjs.org')
-        }
-      }
-    ]
-  }
-];
-
-if (process.platform === 'darwin') {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      {role: 'about'},
-      {type: 'separator'},
-      {role: 'services', submenu: []},
-      {type: 'separator'},
-      {role: 'hide'},
-      {role: 'hideothers'},
-      {role: 'unhide'},
-      {type: 'separator'},
-      {role: 'quit'}
-    ]
-  });
-
-  // Edit menu
-  template[1].submenu.push(
-    {type: 'separator'},
+function initMenu() {
+  const template = [
     {
-      label: 'Speech',
+      label: 'ファイル',
       submenu: [
-        {role: 'startspeaking'},
-        {role: 'stopspeaking'}
+        {
+          label: 'エクスポート',
+          submenu: [
+            {
+              label: 'JSONファイル',
+              click() {
+                exportFile();
+              }
+            },
+            {
+              label: 'HTMLファイル',
+              click() {
+                // TODO
+              }
+            }
+          ]
+        },
+        {
+          label: 'インポート',
+          submenu: [
+            {
+              label: 'JSONファイル',
+              click() {
+                importFile();
+              }
+            }
+          ]
+        }
       ]
     }
-  );
+  ];
 
-  // Window menu
-  template[3].submenu = [
-    {role: 'close'},
-    {role: 'minimize'},
-    {role: 'zoom'},
-    {type: 'separator'},
-    {role: 'front'}
-  ]
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        {role: 'about'},
+        {type: 'separator'},
+        {role: 'services', submenu: []},
+        {type: 'separator'},
+        {role: 'hide'},
+        {role: 'hideothers'},
+        {role: 'unhide'},
+        {type: 'separator'},
+        {role: 'quit'}
+      ]
+    });
+  }
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
 }
-
-const menu = Menu.buildFromTemplate(template);
-Menu.setApplicationMenu(menu);
 
 function exportFile() {
   const options = {
